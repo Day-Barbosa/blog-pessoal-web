ee8b899fe5c73c4b8db0974e84b44979ebb863d9
Dayana Barbosa <day_sbarbosa@outlook.com>
2026-07-06 22:56:04 -0300

    Entrega Deploy

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,39 +1,30 @@
 import ListaPostagens from '../../components/postagem/listapostagens/ListaPostagens'
 import ModalPostagem from '../../components/postagem/modalpostagem/ModalPostagem'
+import IMG_4213 from '../../assets/IMG_4213.jpg';
 
 function Home() {
   return (
     <>
-      <div className="bg-indigo-900 flex justify-center">
-        <div className="container grid grid-cols-2 text-white">
-          
-          <div className="flex flex-col gap-4 items-center justify-center py-4">
-            <h2 className="text-5xl font-bold">
-              Seja Bem Vinde!
-            </h2>
-            <p className="text-xl">
-              Expresse aqui seus pensamentos e opiniões
-            </p>
-            <div className="flex justify-around gap-4">
-              
-             <ModalPostagem />
-
-            </div>
+      <div 
+        className="relative flex justify-center items-center min-h-[50vh] bg-cover bg-center" 
+        style={{ backgroundImage: `url(${IMG_4213})` }}
+      >
+        <div className="absolute inset-0 bg-indigo-900/40"></div>
+        
+        <div className="relative z-10 container text-white flex flex-col items-center justify-center text-center py-16">
+          <h2 className="text-5xl font-bold mb-4 drop-shadow-lg">
+            Seja Bem Vinde!
+          </h2>
+          <p className="text-xl mb-8 drop-shadow-md font-medium">
+            Expresse aqui seus pensamentos e opiniões
+          </p>
+          <div className="flex justify-around gap-4">
+            <ModalPostagem />
           </div>
-
-          <div className="flex justify-center">
-            <img
-              src="https://ik.imagekit.io/gtw1fihay3/undraw_processing-thoughts_wakg.svg"
-              alt="Imagem Página Home"
-              className="w-2/3"
-            />
-          </div>
-
         </div>
       </div>
       
-       <ListaPostagens />
-
+      <ListaPostagens />
     </>
   )
 }
